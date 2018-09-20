68c4f21c64626475376f2e67eb87ca2fe7e9d392
Mitchell Vandehaar <mitchvandehaar@gmail.com>
2018-09-19 18:28:01 -0700

    fixes

--- proxy/server.js
+++ proxy/server.js
@@ -1,11 +1,43 @@
+let express = require('express');
 
-var express = require('express');
-var path = require('path');
-var app = express();
-var port = process.env.PORT || 3000;
+let app = express();
+let parser = require('body-parser');
+let db = require('../info/db/index.js');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(parser.json());
+
+let port = process.env.port || 3000;
+
+app.use(express.static('./public'));
+
+app.get('/restaurants/info/*', (req, res) => {
+  console.log("GET Request on " + req.url);
+  var dbId = req.url.slice(18);
+  db.findOne(dbId, (err, data) => {
+    if (err) {
+      res.status(405).send("error");
+      res.end();
+    } else {
+      res.status(200).send(data);
+      res.end();
+    }
+  });
+});
+
+app.get('/restaurants/all', (req, res) => {
+  console.log("GET Request on " + req.url);
+  db.getAll((err, data) => {
+    if (err) {
+      res.status(404).send("error");
+      res.end();
+    } else {
+      res.status(200).send(data);
+      res.end();
+    }
+  });
+});
+
 
 app.listen(port, () => {
-  console.log(`Listening on ${port}...`);
+  console.log(`Listening on ${port}`);
 });
